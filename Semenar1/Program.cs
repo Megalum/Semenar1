6d27d56ee5d357e00abb860b570b19d4ba3b2dc3
Megaladon <stanko.demon@gmail.com>
2022-06-07 23:58:43 +0300

    modyfied

--- Semenar1/Program.cs
+++ Semenar1/Program.cs
@@ -53,7 +53,7 @@ namespace Semenar1
             Console.WriteLine("Задание 4:");
             Console.Write("Введите число ");
             int n = Convert.ToInt32(Console.ReadLine());
-            for (int i = 2; i < n; i++)
+            for (int i = 2; i <= n; i++)
                 if (i % 2 == 0)
                     Console.Write(i + "; ");
             Console.WriteLine();
